53c2aafb1eb34add0202dc9c0d38d1349a9bd7c6
T.J. Hooker <tyrelhooker@gmail.com>
2018-04-26 22:13:12 -0400

    code import twitter and spotify keys into liri.js

--- liri.js
+++ liri.js
@@ -6,19 +6,21 @@ var Spotify = require('node-spotify-api');
 console.log("Testing dotenv: " + dotenv);
 console.log("Testing process.env: " + process.env.SPOTIFY_ID);
 
+var keys = require("./keys.js");
+// var fs = require("fs");
+// // Stores the contents of the file inside data variable
+// fs.readFile("keys.js", "utf-8", function(error, data) {
+//   if (error) {
+//     return console.log(error);
+//   }
+//   console.log(data);
 
-var fs = require("fs");
-// Stores the contents of the file inside data variable
-fs.readFile("keys.js", "utf-8", function(error, data) {
-  if (error) {
-    return console.log(error);
-  }
-  console.log(data);
-
-});
+// });
 
 var spotify = new Spotify(keys.spotify);
+console.log("\n++++++++++++++++++++++++++++++" + "\nSPOTIFY KEYS: " + "\n" + JSON.stringify(spotify, null, 2));
 var client = new Twitter(keys.twitter);
+console.log("\n++++++++++++++++++++++++++++++" + "\nTWITTER KEYS: " + "\n" + JSON.stringify(client, null, 2));
 
 // TWITTER HERE --------->
 // var client = new Twitter({
